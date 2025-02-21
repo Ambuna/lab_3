5e506efbe42ee4bec17d277a44f31dfd8f845787
ambuna <zolooambuna@gmail.com>
2025-02-21 18:06:17 +0800

    lab 3 bituumjlel

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,12 +1,16 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        // Circle circle = new Circle(3);
+        // Rectangle rectangle = new Rectangle(2, 3);
+        Square square = new Square(3);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer = new Renderer(square);
 
         renderer.draw();
     }
